f0c6630eeca5b6453ab947e686365597c4acc6ae
Eyshcharya <eyshcharya@gmail.com>
2023-12-06 19:15:57 +0530

    Import Prisma/Config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { EmployeesModule } from './employees/employees.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, EmployeesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    EmployeesModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
