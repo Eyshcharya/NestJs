1ade4d91e512270430044665bcd1d38e2da749f7
Eyshcharya <eyshcharya@gmail.com>
2023-12-06 19:18:44 +0530

    Implement get user custom decoration

---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  // get the data passed, get execution context
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    if (data) {
+      return request.user[data];
+    }
+    return request.user;
+  },
+);
