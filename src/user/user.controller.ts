e25a6608168097cb042c71e370ac10cdca7cc87e
Eyshcharya <eyshcharya@gmail.com>
2023-12-07 18:18:11 +0530

    Implemented Patch request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
-import { User } from '@prisma/client';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
+  constructor(private userService: UserService) {}
+
   @Get('me')
   getUser(@GetUser() user: User, @GetUser('email') email: string) {
-    // console.log(email);
+    //  console.log(email);
     return user;
   }
-
   @Patch()
-  editUser() {}
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
 }
