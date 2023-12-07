54edc222a959f3755d7b462d19ec5d2395b008ac
Eyshcharya <eyshcharya@gmail.com>
2023-12-07 18:16:58 +0530

    Added request type

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const GetUser = createParamDecorator(
   // get the data passed, get execution context
   (data: string | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+    const request: Express.Request = ctx.switchToHttp().getRequest();
     if (data) {
       return request.user[data];
     }
